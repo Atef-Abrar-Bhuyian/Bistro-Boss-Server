222b44a4e8801cc8f51aa419ff251734cb3612d0
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-11 16:49:09 +0600

    social login user added to users db

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
     // users related api
     app.post("/users", async (req, res) => {
       const user = req.body;
+      // insert Email if user doesn't exists.
+      // we can do this in many ways(1. email unique, 2. usert, 3. simple checking)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Already Exists", insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
