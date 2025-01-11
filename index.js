8da44efcc94349cb6c604c5893fe6a287f321bb6
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-11 20:58:10 +0600

    admin role functionalities done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
@@ -49,6 +50,26 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
     // get all menu
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
